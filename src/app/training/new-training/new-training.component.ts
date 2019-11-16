405440582912524eb45b750ae5427004ac184b28
maciek77jamer@gmail.com <maciek77jamer@gmail.com>
2019-11-16 16:07:37 +0100

    add id to available exercises items

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,8 +1,10 @@
+import { Exercise } from './../models/exercise.model';
 import { NgForm } from '@angular/forms';
 import { TrainingService } from './../trainingService';
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -10,14 +12,28 @@ import { Observable } from 'rxjs';
   styleUrls: ['./new-training.component.scss']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
 
   constructor(private trainingService: TrainingService,
               private db: AngularFirestore
             ) { }
 
   ngOnInit() {
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+    this.exercises = this.db
+                        .collection('availableExercises')
+                        .snapshotChanges()
+                        .pipe(
+                          map(docArray => {
+                            return docArray.map(doc => {
+                              return {
+                                id: doc.payload.doc.id,
+                                name: doc.payload.doc.data()['name'],
+                                duration: doc.payload.doc.data()['duration'],
+                                calories: doc.payload.doc.data()['calories']
+                              };
+                            });
+                          })
+                        );
   }
 
   onStartTraining(form: NgForm) {
