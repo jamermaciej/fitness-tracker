1122846aa4ba19868466f97d916a98e43f51c25c
maciek77jamer@gmail.com <maciek77jamer@gmail.com>
2019-12-17 20:08:51 +0100

    fixed bugs before build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA5zZk01aq4TmL1VTmQonRJeXfD3udoKHE',
+    authDomain: 'ng-fitness-tracker-91a54.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-91a54.firebaseio.com',
+    projectId: 'ng-fitness-tracker-91a54',
+    storageBucket: 'ng-fitness-tracker-91a54.appspot.com',
+    messagingSenderId: '915146444462',
+    appId: '1:915146444462:web:1a1644c96394f61f2be73f',
+    measurementId: 'G-2447V1TVBC'
+  }
 };
